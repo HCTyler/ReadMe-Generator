4248800092f952d1da2e17fa6b32aca93aef3a16
Edgar Fernandez <tempurakohi@gmail.com>
2022-02-08 22:57:06 -0800

    made questions and template of a README

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
+const fs= require("fs")
+const path= require("path")
+const inquirer= require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name:"github",
+        message:"What is your Github username?"
+    },
+    {
+        type:"input",
+        name:"email",
+        message:"what is your email address?"
+    },
+    {
+        type:"input",
+        name:"title",
+        message:"What is your project's name?"
+    },
+    {
+        type:"input",
+        name:"description",
+        messege:"Please write a description of your project"
+    },
+    {
+        type:"list",
+        name:"tcontent",
+        messege:"Would you like to include a Table of Content?",
+        choices:["Yes","No"]
+    },
+    {
+        type:"input",
+        name:"installation",
+        messege:"What are the command steps required to install your project?"
+
+    },
+    {
+        type:"input",
+        name:"usage",
+        messege:"Please provide instructions and examples of use."
+    },
+    {
+        type:"input",
+        name:"collaborator",
+        messege:"List your collaborators or tutorials that were helpful."
+    },
+    {
+        type:"list",
+        name:"license",
+        message:"What license(s) should your project have?",
+        choices:["MIT","APACHE 2.0","GPL 3.0","None"]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeToSync(path.join(process.cwd(),fileName),data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses)=>{
+        console.log("Generating a README file...")
+        console.log("... loading...")
+        writeToFile("README.md", generateMarkdown)
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,76 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== "None") {
+    return `![Github License]((https://img.shields.io/badge/license-${license}-blue.svg))`
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license !== "none") {
+    return `\n* [License](#license)\n`
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License
+    
+This project is licensed under the ${license} license.`
+  }
+  return ""
+}
 
+function renderTableOfContent(tcontent){
+  if(tcontent !=="No") {
+    return `## Table of Content
+    
+[Installation](#installation)
+[Usage](#usage)
+[Credits](#credits)
+${renderLicenseLink(data.license)}
+
+`
+  }
+  return ""
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+ 
+## Description
+
+${data.description}
+
+${renderTableOfContent(data.tcontent)}
+
+## Installation
+
+To install necessary dependencies for this project, run the following command:
+
+> ${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.collaborators}
+
+${renderLicenseBadge(data.license)}
+
+## Questions
+
+If you have any other questions about the repo, contact me at ${data.email.com}. You can find more of my work at[${data.github}](https://github.com/${data.guthub})
+
+
 
 `;
 }
